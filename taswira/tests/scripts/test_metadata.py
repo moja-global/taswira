4a29b428b24dbbf2bbb4be08eb91aaf072a61156
Abhineet Tamrakar <abhineet97@gmail.com>
2020-06-23 19:03:13 +0530

    Remove forgotten assert statement
    Signed-off-by: Abhineet Tamrakar <abhineet97@gmail.com>

--- taswira/tests/scripts/test_metadata.py
+++ taswira/tests/scripts/test_metadata.py
@@ -8,5 +8,3 @@ def test_metadata(set_config, GCBM_compiled_output):
     assert not len(metadata) == 0
     for _, indicator_values in metadata.items():
         assert not len(indicator_values) == 0
-
-    assert 0
